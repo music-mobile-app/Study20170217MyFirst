0a9559b20c6d394e8df63d29eb4abfb779204a42
music-mobile-app <music@nw-exam.com>
2017-02-17 00:37:57 +0900

    コミットメッセージ

--- MyFirst/ViewController.swift
+++ MyFirst/ViewController.swift
@@ -13,13 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      outletLabel.text = "Hello Swift!"
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+      
+
     }
 
 
+  @IBOutlet weak var outletLabel: UILabel!
 }
 
